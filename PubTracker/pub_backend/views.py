4073127c1f2c9e6d8d6c06f9b9936baeccb17da2
gubrian <gubrian@gubrian.attlocal.net>
2015-05-02 00:47:52 -0700

    Add request for rss parser

--- PubTracker/pub_backend/views.py
+++ PubTracker/pub_backend/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import rsshandler
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def rss(request):
+    return rsshandler.rss_parse('')
+
 
